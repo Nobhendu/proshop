f997f6bea1586d93218da75cd88d655e37394b8d
Nobhendu <96724265+Nobhendu@users.noreply.github.com>
2023-07-25 19:36:12 +0530

    Create HomeScreen component and list products

---
+++ frontend/src/components/Product.tsx
@@ -0,0 +1,36 @@
+import { Card } from "react-bootstrap";
+
+interface productType {
+  _id: string;
+  name: string;
+  image: string;
+  description: string;
+  brand: string;
+  category: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+}
+
+const Product = ({ product }: { product: productType }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
