37c9c29c48196c8470b4e8db300fd10bc7c91e33
Nobhendu <96724265+Nobhendu@users.noreply.github.com>
2023-07-30 15:13:46 +0530

    Create auth and user API slices

---
+++ frontend/src/slices/authSlice.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { userType } from "../../typings/*";
+
+const initialState: { userInfo: userType } = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo") as string)
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadAction<userType>) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+export default authSlice.reducer;
