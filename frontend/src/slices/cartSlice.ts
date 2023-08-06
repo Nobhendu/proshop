f5fd592709ad2f4466f70050d24c7a6876eea9eb
Nobhendu <96724265+Nobhendu@users.noreply.github.com>
2023-08-06 12:28:31 +0530

    Create shipping screen

--- frontend/src/slices/cartSlice.ts
+++ frontend/src/slices/cartSlice.ts
@@ -2,9 +2,9 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { itemType } from "../../typings/*";
 import { updateCart } from "../utils/cartUtils";
 
-const initialState: itemType = localStorage.getItem("cart")
+const initialState: any = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart")!)
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "Paypal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -13,9 +13,11 @@ const cartSlice = createSlice({
     addToCart: (state, action: PayloadAction<itemType>) => {
       const item = action.payload;
 
-      const existItem = state.cartItems.find((x) => x._id === item._id);
+      const existItem = state.cartItems.find(
+        (x: itemType) => x._id === item._id
+      );
       if (existItem)
-        state.cartItems = state.cartItems.map((x) =>
+        state.cartItems = state.cartItems.map((x: itemType) =>
           x._id === existItem._id ? item : x
         );
       else state.cartItems = [...state.cartItems, item];
@@ -23,13 +25,20 @@ const cartSlice = createSlice({
       updateCart(state);
     },
     removeFromCart: (state, action: PayloadAction<string>) => {
-      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      state.cartItems = state.cartItems.filter(
+        (x: itemType) => x._id !== action.payload
+      );
 
       updateCart(state);
+    },
+    saveShippingAddress: (state, action: PayloadAction<any>) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
     },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
